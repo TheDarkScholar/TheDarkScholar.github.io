df8594bbb20e8daaf6c51d33f00db7e608a28086
Michael Fourie <michaellfourie@gmail.com>
2018-02-07 15:19:41 -0600

    added basicinteraction

---
+++ basicinteraction/script.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+}
+
+function draw() {
+  background(255)
+
+  rect(50,50,90,90)
+}
---
+++ basicinteraction/sketch.js
@@ -0,0 +1,22 @@
+//basciinteraction
+// Michael Fourie- Feb 7 2018
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}
+
+function keyPressed() {
+  noStroke();
+  fill(random(255), random(255), random(255), random(255));
+  ellipse(random(0, windowWidth), random(0, windowHeight), random(50, 200), random(50, 200));
+}
+
+function mouseClicked() {
+  noStroke();
+  fill(random(255), random(255), random(255), random(255));
+  rect(mouseX, mouseY, random(50, 200), random(50, 200));
+}
