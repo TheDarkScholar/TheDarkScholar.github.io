9212bd9192e57cdadde3f4df8746026e70d783f4
Michael Fourie <michaellfourie@gmail.com>
2018-02-09 14:47:53 -0600

    started on interactive scene from keyboardandmouse example

---
+++ interactivescen/sketch.js
@@ -0,0 +1,35 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  document : addEventListener("contextmenu", event => event.preventDefault());
+}
+
+function draw() {
+
+}
+
+function mousePressed() {
+  noStroke();
+  fill(random(255), random(255), random(255), random(255));
+  if (mouseButton === LEFT) {
+    rect(mouseX, mouseY, random(50, 150), random(50, 150));
+  }
+  else if (mouseButton === RIGHT) {
+    ellipse(mouseX, mouseY, random(255), random(255));
+  }
+}
+
+function keyPressed() {
+  if (key === "w" || key === "W") {
+    background(255);
+  }
+  else if (key === "b" || key === "B") {
+    background(0);
+  }
+}
+
+function deviceShaken() {
+  fill(random(255),random(255),random(255),random(255));
+  textFont("Times New Roman");
+  textSize(random(32, 100));
+  text("Hello there",random(width),random(height));
+}
