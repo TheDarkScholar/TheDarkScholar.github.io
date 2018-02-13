5434766e39e04e8724635d665cb808a5799b6d43
Michael Fourie <michaellfourie@gmail.com>
2018-02-13 15:20:15 -0600

    edited scen

--- interactivescen/sketch.js
+++ interactivescen/sketch.js
@@ -1,5 +1,6 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
   document: addEventListener("contextmenu", event => event.preventDefault());
 }
 
@@ -9,22 +10,14 @@ function draw() {
 
 function mousePressed() {
   noStroke();
-  fill(random(255), random(255), random(255), random(255));
-  if (mouseButton === LEFT) {
-    rect(mouseX, mouseY, random(50, 150), random(50, 150));
-  }
-  else if (mouseButton === RIGHT) {
-    ellipse(random(255), random(255), random(255), random(255));
+  fill(random(255), random(255), random(255));
+  if (mouseButton === RIGHT || mouseButton === LEFT) {
+    ellipse(random(255), random(255), 1, 1);
   }
 }
 
 function keyPressed() {
-  if (key === "w" || key === "W") {
-    background(255);
-  }
-  else if (key === "b" || key === "B") {
-    background(0);
-  }
+
 }
 
 function deviceShaken() {
