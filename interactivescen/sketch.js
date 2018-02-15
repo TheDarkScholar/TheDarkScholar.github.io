9173b1517e805e3854035a8806b5ed81fbaff975
Michael Fourie <michaellfourie@gmail.com>
2018-02-15 15:20:23 -0600

    wotkred scen

--- interactivescen/sketch.js
+++ interactivescen/sketch.js
@@ -1,8 +1,8 @@
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
   document: addEventListener("contextmenu", event => event.preventDefault());
+
 }
 
 function draw() {
@@ -10,16 +10,25 @@ function draw() {
 }
 
 function mousePressed() {
-
-
+  if (mouseButton === RIGHT) {
+    background(0);
+  }
 }
 
 function keyPressed() {
   noStroke();
-  fill(255,255,255);
-
+  fill(255, 255, 255, random(255));
   if (key === "E" || key === "e") {
-    rect(random(windowWidth), random(windowHeight), 5, 5);
+    for (let i = 0; i < 1500; i++) {
+      ellipse(random(windowWidth), random(windowHeight), 2, 2);
+    }
+  }
+  if (key === "N" || key === "n") {
+    for (var i = 0; i < 500; i++) {
+      fill(random(255), random(255), random(255), random(255));
+      ellipse(random(windowWidth), random(windowHeight), 2, 2);
+
+    }
   }
 }
 
