d5b04989c69c6665a42c50301add747c0f9f8fc1
Michael Fourie <michaellfourie@gmail.com>
2018-02-16 15:20:04 -0600

    updated scen

--- interactivescen/sketch.js
+++ interactivescen/sketch.js
@@ -20,21 +20,21 @@ function keyPressed() {
   fill(255, 255, 255, random(255));
   if (key === "E" || key === "e") {
     for (let i = 0; i < 1500; i++) {
-      ellipse(random(windowWidth), random(windowHeight), 2, 2);
+      ellipse(random(windowWidth), random(windowHeight), 3, 3);
     }
   }
   if (key === "N" || key === "n") {
     for (var i = 0; i < 500; i++) {
       fill(random(255), random(255), random(255), random(255));
-      ellipse(random(windowWidth), random(windowHeight), 2, 2);
-
+      ellipse(random(windowWidth), random(windowHeight), 4, 4);
     }
   }
 }
 
+
 function deviceShaken() {
-  fill(random(255), random(255), random(255), random(255));
+  fill(random(100, 200), random(100, 200), random(255), random(255));
   textFont("Times New Roman");
-  textSize(random(32, 100));
-  text("Hello There", random(width), random(height));
+  textSize(random(20, 32));
+  text("SPAAAAAAAAAAAAAAAACE", random(width), random(height));
 }
