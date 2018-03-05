593a7b04d36199a43c917026da1e8d878a4e74f1
Michael Fourie <michaellfourie@gmail.com>
2018-03-05 15:20:11 -0600

    dot update and object example added

--- dottboard/sketch.js
+++ dottboard/sketch.js
@@ -1,19 +1,21 @@
-
 function setup() {
-  if (windowWidth > windowHeight){
-    createCanvas(windowWidth,windowWidth);
-  }
-  else {
-    createCanvas(windowHeight,windowHeight);
-  }
+  createCanvas(windowWidth, windowHeight);
+  noCursor();
 }
 
 function draw() {
   background(0);
+  drawDots();
 }
 
-function drawDots(){
-  for (let i = 0; i < 50; i ++){
-    for (let j= 0;)
+function drawDots() {
+  let pointSpace = 50;
+  for (let x = pointSpace; x < width; x += pointSpace) {
+    for (let y = pointSpace; y < height; y += pointSpace) {
+      fill(0,random(255), 0);
+      ellipse(x, y, 4, 4);
+      stroke(0,random(255), 0, 80);
+      line(x,y,random(0,width),random(0,height));
+    }
   }
 }
