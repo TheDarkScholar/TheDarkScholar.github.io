9d8d6d75820ccd85aa368774eba680c42cdcd8c9
Michael Fourie <michaellfourie@gmail.com>
2018-03-14 15:21:10 -0600

    update statvars

--- stateVariables/sketch.js
+++ stateVariables/sketch.js
@@ -1,18 +1,6 @@
 let nebularea;
 let nebula;
-// shapes of nebula
-let nebula1 ={
-  area: random(width)/2,
-  area2: random(height)/ 2
-};
-let nebula2 ={
-  area: random(width)/4,
-  area2: random(height)/4
-};
-let nebula3 ={
-  area: random(width)/6,
-  area2: random(height)/6
-};
+
 //changes the type of nebula(colour)
 let state;
 
@@ -21,8 +9,22 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   let state = 1;
   let nebula = state;
-  let nebularea = [nebula1,nebula2,nebula3];
+  // shapes of nebula
+  let nebula1 ={
+    area: width/2,
+    area2: height/ 2
+  };
+  let nebula2 ={
+    area: width/4,
+    area2: height/4
+  };
+  let nebula3 ={
+    area: width/6,
+    area2: height/6
+  };
+  let nebularea = [nebula1.area,nebula2.area,nebula3.area,nebula1.area2,nebula2.area2,nebula3.area2];
 }
+
 
 function draw() {
 
@@ -31,6 +33,30 @@ function draw() {
 function mousePressed() {
   if (mouseButton === RIGHT) {
     background(0);
+  }
+}
+
+
+function createNebulae() {
+  if (nebula === 1){
+    fill(random(255),0, 0, random(255));
+    ellipse(random(nebularea[0],nebularea[2]), random(nebularea[3],nebularea[5]), 4, 4);
+    state = random(1,4);
+  }
+  else if (nebula === 2){
+    fill(0, random(255), 0, random(255));
+    ellipse(random(nebularea[0],nebularea[2]), random(nebularea[3],nebularea[5]), 4, 4);
+    state = random(1,4);
+  }
+  else if (nebula === 3){
+    fill(0, 0, random(255), random(255));
+    ellipse(random(nebularea[0],nebularea[2]), random(nebularea[3],nebularea[5]), 4, 4);
+    state = random(1,4);
+  }
+  else if (nebula === 4){
+    fill(237, 147, 53, random(255));
+    ellipse(random(nebularea[0],nebularea[2]), random(nebularea[3],nebularea[5]), 4, 4);
+    state = random(1,4);
   }
 }
 
@@ -49,28 +75,6 @@ function keyPressed() {
   }
 }
 
-function createNebulae(){
-  if (nebula === 1){
-    fill(random(255),0, 0, random(255));
-    ellipse(random(nebularea[0],nebularea[2]), random(nebularea[0],nebularea[2]), 4, 4);
-    state = random(1,4);
-  }
-  else if (nebula === 2){
-    fill(0, random(255), 0, random(255));
-    ellipse(random(nebularea[0],nebularea[2]), random(nebularea[0],nebularea[2]), 4, 4);
-    state = random(1,4);
-  }
-  else if (nebula === 3){
-    fill(0, 0, random(255), random(255));
-    ellipse(random(nebularea[0],nebularea[2]), random(nebularea[0],nebularea[2]), 4, 4);
-    state = random(1,4);
-  }
-  else if (nebula === 4){
-    fill(237, 147, 53, random(255));
-    ellipse(random(nebularea[0],nebularea[2]), random(nebularea[0],nebularea[2]), 4, 4);
-    state = random(1,4);
-  }
-}
 
 // function deviceShaken() {
 //   fill(random(100, 200), random(100, 200), random(255), random(255));
