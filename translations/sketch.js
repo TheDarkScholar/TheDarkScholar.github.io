1329e22834531c093a58460515f54f65d43794cc
Michael Fourie <michaellfourie@gmail.com>
2018-03-14 15:06:11 -0600

    updated trans

--- translations/sketch.js
+++ translations/sketch.js
@@ -1,20 +1,40 @@
+let clocksize;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  if (width>height){
+    clocksize=height*0.9;
+  }
+  else{
+    clocksize=width*0.9;
+  }
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(255);
   push();
-  angleMode(DEGREES);
-
+  //circle
   translate(width/2,height/2);
-  ellipse(0,0,400,400);
-  rotate(frameCount*0.25);
-  fill(random(255),random(255),random(255));
-  rectMode(CENTER);
-  rect(0,0,300,50);
-  pop();
+  strokeWeight(10);
+  ellipse(0,0,clocksize,clocksize);
+  //middle
+  fill(0);
+  ellipse(0,0,4,4);
+  //hours
+  strokeWeight(10);
+  strokeCap(SQUARE);
+  for(let i = 0; i<12; i++){
+    line(0,clocksize/2*0.95,0,clocksize/2*0.75);
+    rotate(360/12);
+  }
+  //minutes
+  strokeWeight(4);
+  strokeCap(SQUARE);
+  for(let i = 0; i<60; i++){
+    line(0,clocksize/2*0.95,0,clocksize/2*0.80);
+    rotate(360/60);
+  }
 
-  rect(100,100,200,200);
+  pop();
 
 }
