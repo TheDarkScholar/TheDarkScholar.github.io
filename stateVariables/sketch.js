5b5b6f5f5a85c5c3354af1db112501dd86f2915d
Michael Fourie <michaellfourie@gmail.com>
2018-03-16 15:19:57 -0600

    update statevars

--- stateVariables/sketch.js
+++ stateVariables/sketch.js
@@ -1,26 +1,25 @@
-let nebularea;
 let nebula;
 //changes the type of nebula(colour)
 let state;
+// shapes of nebula
+let nebula1 = {
+  area: random(windowWidth / 2),
+  area2: random(windowHeight / 2)
+};
+let nebula2 = {
+  area: width / 4,
+  area2: height / 4
+};
+let nebula3 = {
+  area: width / 6,
+  area2: height / 6
+};
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   let state = 1;
   let nebula = state;
-  // shapes of nebula
-  let nebula1 ={
-    area: width/2,
-    area2: height/2
-  };
-  let nebula2 ={
-    area: width/4,
-    area2: height/4
-  };
-  let nebula3 ={
-    area: width/6,
-    area2: height/6
-  };
-  let nebularea = [nebula1.area,nebula2.area,nebula3.area,nebula1.area2,nebula2.area2,nebula3.area2];
+
 }
 
 function draw() {
@@ -34,29 +33,29 @@ function mousePressed() {
 }
 
 function createNebulae() {
-  if (nebula === 1){
-    fill(random(255),0, 0, random(255));
-    ellipse(random(nebularea[0],nebularea[2]), random(nebularea[3],nebularea[5]), 3, 3);
-    state = random(1,4);
-    return state;
+  if (nebula === 1) {
+    fill(random(255), 0, 0, random(255));
+    ellipse(nebula1.area, nebula1.area2, 3, 3);
+    nebula = random(1, 4);
+    return nebula;
   }
-  else if (nebula === 2){
+  else if (nebula === 2) {
     fill(0, random(255), 0, random(255));
-    ellipse(random(nebularea[0],nebularea[2]), random(nebularea[3],nebularea[5]), 3, 3);
-    state = random(1,4);
-    return state;
+    ellipse(random(windowWidth), random(windowHeight), 3, 3);
+    nebula = random(1, 4);
+    return nebula;
   }
-  else if (nebula === 3){
+  else if (nebula === 3) {
     fill(0, 0, random(255), random(255));
-    ellipse(random(nebularea[0],nebularea[2]), random(nebularea[3],nebularea[5]), 3, 3);
-    state = random(1,4);
-    return state;
+    ellipse(random(windowWidth), random(windowHeight), 3, 3);
+    nebula = random(1, 4);
+    return nebula;
   }
-  else if (nebula === 4){
+  else if (nebula === 4) {
     fill(237, 147, 53, random(255));
-    ellipse(random(nebularea[0],nebularea[2]), random(nebularea[3],nebularea[5]), 3, 3);
-    state = random(1,4);
-    return state;
+    ellipse(random(windowWidth), random(windowHeight), 3, 3);
+    nebula = random(1, 4);
+    return nebula;
   }
 }
 
@@ -70,6 +69,7 @@ function keyPressed() {
   }
   if (key === "N" || key === "n") {
     for (let i = 0; i < 500; i++) {
+      nebula = 1;
       createNebulae();
     }
   }
