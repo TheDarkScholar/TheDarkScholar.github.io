7c8096cf77aceec64b63e3fdd191ac1cbde96d24
Michael Fourie <michaellfourie@gmail.com>
2018-03-20 15:19:13 -0600

    added heighest point finding

--- terraingendemo/sketch.js
+++ terraingendemo/sketch.js
@@ -1,28 +1,52 @@
 let heights = [];
-let numberORectangles=1000000;
+let numberORectangles = 5000000;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   numberORectangles = width;
   generateInitialTerrain(numberORectangles);
+
 }
 
 function draw() {
   background(255);
   displayTerrain();
-
+  plantFlag();
 }
 
-function generateInitialTerrain(numberORectangles){
-  for (let i = 0; i<numberORectangles; i++){
-    heights.push(random(height/2));
+function generateInitialTerrain(numberORectangles) {
+  let time = 0;
+  let ct = 0.01;
+
+  for (let i = 0; i < numberORectangles; i++) {
+    let currentHeight= noise(time)*500;
+    heights.push(currentHeight);
+    time +=ct;
   }
 }
+
 function displayTerrain() {
-  let rectWidth = width/numberORectangles;
+  let rectWidth = width / numberORectangles;
   rectMode(CORNERS);
-  for (let i = 0; i <numberORectangles; i ++){
-    rect(i*rectWidth,height,(i+1)*rectWidth,height-heights[i]);
+  stroke(0);
+  for (let i = 0; i < numberORectangles; i++) {
+    rect(i * rectWidth, height, (i + 1) * rectWidth, height - heights[i]);
   }
+}
+
+function plantFlag(){
+  let tallest = 0;
+  let heighestX;
+  let rectWidth = width / numberORectangles;
+  for (let i = 0; i < heights.length; i++){
+    if (heights[i] > tallest){
+      tallest = heights[i];
+      heighestX = i*rectWidth;
+    }
+  }
+  let heighestY = height - tallest;
+  stroke(255,0,0);
+  line(0,heighestY,width,heighestY);
+  ellipse(heighestX,heighestY,10,10);
 }
