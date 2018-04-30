bf43b3595f1f47311495206a4eae1b3248df7975
Michael Fourie <michaellfourie@gmail.com>
2018-04-30 14:32:49 -0600

    change

--- testingGrounds/sketch.js
+++ testingGrounds/sketch.js
@@ -1,16 +1,9 @@
-let player1, player2, player3, player4, player5, player6, player7, player8;
+let player;
 let characterXPos = 100;
 let characterYPos = 100;
 
 function preload() {
-  player1 = loadImage("images/armor-move-1.png");
-  player2 = loadImage("images/armor-move-2.png");
-  player3 = loadImage("images/armor-move-3.png");
-  player4 = loadImage("images/armor-move-4.png");
-  player5 = loadImage("images/armor-move-5.png");
-  player6 = loadImage("images/armor-move-6.png");
-  player7 = loadImage("images/armor-move-7.png");
-  player8 = loadImage("images/armor-move-8.png");
+  player = loadImage("images/armor-move-2.png");
 }
 
 function setup() {
@@ -18,29 +11,25 @@ function setup() {
 }
 
 function draw() {
-  playerSpriteAnimation();
+  mouseClicked();
 }
 
-function playerSpriteAnimation() {
-  let playerSprite = [player1, player2, player3, player4, player5, player6, player7, player8];
-  if (mouseIsPressed && mouseButton === RIGHT) {
+function mouseClicked() {
+  if (mouseClicked && mouseButton === LEFT) {
     let clickedMouseX = mouseX;
     let clickedMouseY = mouseY;
     for (let x = characterXPos; x < clickedMouseX; x++) {
       for (let y = characterYPos; y < clickedMouseY; y++) {
-        for (let i = 0; i < playerSprite.length; i++) {
-          image(playerSprite[i], characterXPos, characterYPos);
-          if (i > playerSprite.length) {
-            i = 0;
-          }
-        }
+        y += 10;
         if (y === clickedMouseY) {
           let characterYPos = clickedMouseY;
         }
       }
+      x += 10;
       if (x === clickedMouseX) {
         let characterXPos = clickedMouseX;
       }
     }
+    image(player, characterXPos, characterYPos);
   }
 }
