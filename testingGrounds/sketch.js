404bfab6f2f6a8e259b9cfde54d6c73ca021e0fa
Michael Fourie <michaellfourie@gmail.com>
2018-04-30 15:20:51 -0600

    added testg2, started health system and potions

--- testingGrounds/sketch.js
+++ testingGrounds/sketch.js
@@ -1,35 +1,87 @@
 let player;
 let characterXPos = 100;
 let characterYPos = 100;
+let health = 20;
+let healthPotion;
+let r = 255;
+let g = 255;
+let b = 255;
 
 function preload() {
-  player = loadImage("images/armor-move-2.png");
+  player = loadImage("images/armor-move-4.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
-  mouseClicked();
+  background(255);
+  image(player, characterXPos, characterYPos);
+  playerHealth();
+  // mousePressed();
 }
 
-function mouseClicked() {
-  if (mouseClicked && mouseButton === LEFT) {
-    let clickedMouseX = mouseX;
-    let clickedMouseY = mouseY;
-    for (let x = characterXPos; x < clickedMouseX; x++) {
-      for (let y = characterYPos; y < clickedMouseY; y++) {
-        y += 10;
-        if (y === clickedMouseY) {
-          let characterYPos = clickedMouseY;
-        }
-      }
-      x += 10;
-      if (x === clickedMouseX) {
-        let characterXPos = clickedMouseX;
-      }
-    }
-    image(player, characterXPos, characterYPos);
+// function mousePressed() {
+//   if (mouseIsPressed && mouseButton === LEFT) {
+//     let clickedMouseX = mouseX;
+//     let clickedMouseY = mouseY;
+//     for (let x = characterXPos; x < clickedMouseX; x++) {
+//       for (let y = characterYPos; y < clickedMouseY; y++) {
+//         if (y === clickedMouseY) {
+//           let characterYPos = clickedMouseY;
+//         }
+//       }
+//       if (x === clickedMouseX) {
+//         let characterXPos = clickedMouseX;
+//       }
+//     }
+//   }
+// }
+
+function keyPressed(){
+  if (key === "w" || key === "W"){
+    characterYPos -= 10;
+  }
+  else if (key === "a" || key === "A"){
+    characterXPos -= 10;
+  }
+  else if (key === "s" || key === "S"){
+    characterYPos += 10;
+  }
+  else if (key === "d" || key === "D"){
+    characterXPos += 10;
+  }
+}
+
+function playerHealth(){
+  if (characterXPos > 300 || characterYPos > 300){
+    health -= 0.5;
+  }
+  if (health < 1){
+    rect(characterXPos,characterYPos,30,30);
+    fill(0);
+  }
+  else if (characterXPos ){
+    let d;
+  }
+}
+
+class HealthPotion{
+  constructor(){
+    this.potionwidth;
+    this.potionheight;
+    this.potioncolor;
+    this.potionx;
+    this.potiony;
+    this.potiontop;
+    this.potionbot;
+    this.potionlft;
+    this.potionrgt;
+  }
+  display(){
+    fill(random(r),random(g),random(b));
+    ellipse(this.potionx,this.potiony,this.potionwidth, this.potionheight);
   }
 }
