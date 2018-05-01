b25f6e809e2604f31fe09732e2903afe4a5bafae
Michael Fourie <michaellfourie@gmail.com>
2018-05-01 15:21:55 -0600

    worked on potions

--- testingGrounds/sketch.js
+++ testingGrounds/sketch.js
@@ -3,9 +3,7 @@ let characterXPos = 100;
 let characterYPos = 100;
 let health = 20;
 let healthPotion;
-let r = 255;
-let g = 255;
-let b = 255;
+
 
 function preload() {
   player = loadImage("images/armor-move-4.png");
@@ -13,74 +11,75 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+  healthPotion = new Potion(random(400), random(400));
 }
 
 function draw() {
   background(255);
   image(player, characterXPos, characterYPos);
   playerHealth();
-  // mousePressed();
+  stroke(0);
+  fill(255,255,255,0);
+  rect(0,0,300,300);
+  healthPotion.display();
 }
 
-// function mousePressed() {
-//   if (mouseIsPressed && mouseButton === LEFT) {
-//     let clickedMouseX = mouseX;
-//     let clickedMouseY = mouseY;
-//     for (let x = characterXPos; x < clickedMouseX; x++) {
-//       for (let y = characterYPos; y < clickedMouseY; y++) {
-//         if (y === clickedMouseY) {
-//           let characterYPos = clickedMouseY;
-//         }
-//       }
-//       if (x === clickedMouseX) {
-//         let characterXPos = clickedMouseX;
-//       }
-//     }
-//   }
-// }
-
-function keyPressed(){
-  if (key === "w" || key === "W"){
-    characterYPos -= 10;
+class Potion {
+  constructor(x, y) {
+    this.potionradius = 20;
+    this.potiontop = y;
+    this.potionbot = this.potiontop + this.potionradius;
+    this.potionlft = x;
+    this.potionrgt = this.lft + this.potionradius;
+    this.r = random(255);
+    this.g = random(255);
+    this.b = random(255);
   }
-  else if (key === "a" || key === "A"){
-    characterXPos -= 10;
-  }
-  else if (key === "s" || key === "S"){
-    characterYPos += 10;
-  }
-  else if (key === "d" || key === "D"){
-    characterXPos += 10;
+  display() {
+    noStroke();
+    fill(this.r, this.g, this.b);
+    ellipse(this.potionlft, this.potiontop, this.potionradius, this.potionradius);
   }
 }
 
-function playerHealth(){
-  if (characterXPos > 300 || characterYPos > 300){
-    health -= 0.5;
+function keyPressed() {
+  if (key === "w" || key === "W") {
+    characterYPos -= 20;
   }
-  if (health < 1){
-    rect(characterXPos,characterYPos,30,30);
+  else if (key === "a" || key === "A") {
+    characterXPos -= 20;
+  }
+  else if (key === "s" || key === "S") {
+    characterYPos += 20;
+  }
+  else if (key === "d" || key === "D") {
+    characterXPos += 20;
+  }
+  else if (key === "e" || key === "E") {
+    let xcoord = floor(this.potionlft/this.potionradius);
+    let ycoord = floor(this.potiontop/this.potionradius);
+    if (characterXPos === xcoord && characterYPos === ycoord){
+      if (this.r > this.g && this.r > this.b) {
+        health = health + 5;
+      }
+      else if (this.g > this.r && this.g > this.b) {
+        health = health -= 5;
+      }
+      else if (this.b > this.g && this.b > this.r) {
+        health = health + 10;
+      }
+    }
+  }
+}
+
+function playerHealth() {
+  if (characterXPos > 300 || characterYPos > 300) {
+    health -= 0.05;
+  }
+  if (health < 1) {
     fill(0);
-  }
-  else if (characterXPos ){
-    let d;
+    rect(characterXPos, characterYPos, 30, 30);
   }
 }
 
-class HealthPotion{
-  constructor(){
-    this.potionradius = 10;
-    this.potioncolor = (random(r),random(g),random(b));
-    this.potionx= random(windowWidth);
-    this.potiony = random(windowHeight);
-    this.potiontop;
-    this.potionbot;
-    this.potionlft;
-    this.potionrgt;
-  }
-  display(){
-    fill(this.potioncolor);
-    ellipse(this.potionx,this.potiony,this.potionradius,this.potionradius);
-  }
-}
+// characterXPos >= this.potionlft && characterXPos <= this.potionrgt && characterYPos >= this.potiontop && characterYPos <= this.potionbot
