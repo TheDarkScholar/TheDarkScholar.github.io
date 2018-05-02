f8799182b79224e4bedfdfbd1ce223104364136f
Michael Fourie <michaellfourie@gmail.com>
2018-05-02 15:19:47 -0600

    updated

--- testingGrounds/sketch.js
+++ testingGrounds/sketch.js
@@ -1,9 +1,9 @@
 let player;
 let characterXPos = 100;
 let characterYPos = 100;
-let health = 20;
+let health = 200;
 let healthPotion;
-
+let poisonPool;
 
 function preload() {
   player = loadImage("images/armor-move-4.png");
@@ -11,36 +11,56 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  healthPotion = new Potion(random(400), random(400));
+  // healthPotion = new Potion(random(400), random(400));
+  // poisonPool = new Poison(random(300), random(300));
 }
 
 function draw() {
   background(255);
+  stroke(0);
+  fill(255, 255, 255, 0);
+  rect(0, 0, 300, 300);
+  // healthPotion.display();
+  // poisonPool.display();
   image(player, characterXPos, characterYPos);
   playerHealth();
-  stroke(0);
-  fill(255,255,255,0);
-  rect(0,0,300,300);
-  healthPotion.display();
 }
 
-class Potion {
-  constructor(x, y) {
-    this.potionradius = 20;
-    this.potiontop = y;
-    this.potionbot = this.potiontop + this.potionradius;
-    this.potionlft = x;
-    this.potionrgt = this.lft + this.potionradius;
-    this.r = random(255);
-    this.g = random(255);
-    this.b = random(255);
-  }
-  display() {
-    noStroke();
-    fill(this.r, this.g, this.b);
-    ellipse(this.potionlft, this.potiontop, this.potionradius, this.potionradius);
-  }
-}
+// class Potion {
+//   constructor(x, y) {
+//     this.potionradius = 20;
+//     this.potiontop = y;
+//     this.potionbot = this.potiontop + this.potionradius;
+//     this.potionlft = x;
+//     this.potionrgt = this.potionlft + this.potionradius;
+//     this.potionr = random(255);
+//     this.potiong = 0;
+//     this.potionb = random(255);
+//   }
+//   display() {
+//     noStroke();
+//     fill(this.potionr, this.potiong, this.potionb);
+//     ellipse(this.potionlft, this.potiontop, this.potionradius, this.potionradius);
+//   }
+// }
+//
+// class Poison {
+//   constructor(x, y) {
+//     this.poisonradius = random(40, 80);
+//     this.poisontop = y;
+//     this.poisonbot = this.poisontop + this.poisonradius;
+//     this.poisonlft = x;
+//     this.poisonrgt = this.poisonlft + this.poisonradius;
+//     this.poisonr = 0;
+//     this.poisong = random(50, 255);
+//     this.poisonb = 0;
+//   }
+//   display() {
+//     noStroke();
+//     fill(this.poisonr, this.poisong, this.poisonb);
+//     ellipse(this.poisonlft, this.poisontop, this.poisonradius, this.poisonradius);
+//   }
+// }
 
 function keyPressed() {
   if (key === "w" || key === "W") {
@@ -55,31 +75,37 @@ function keyPressed() {
   else if (key === "d" || key === "D") {
     characterXPos += 20;
   }
-  else if (key === "e" || key === "E") {
-    let xcoord = floor(this.potionlft/this.potionradius);
-    let ycoord = floor(this.potiontop/this.potionradius);
-    if (characterXPos === xcoord && characterYPos === ycoord){
-      if (this.r > this.g && this.r > this.b) {
-        health = health + 5;
-      }
-      else if (this.g > this.r && this.g > this.b) {
-        health = health -= 5;
-      }
-      else if (this.b > this.g && this.b > this.r) {
-        health = health + 10;
-      }
-    }
-  }
+  // else if (key === "e" || key === "E") {
+  //   let xcoord = floor(this.potionlft / this.potionradius);
+  //   let ycoord = floor(this.potiontop / this.potionradius);
+  //   if (characterXPos === xcoord && characterYPos === ycoord) {
+  //     if (this.potionr > this.potionb) {
+  //       health += 5;
+  //       this.potionr = 255;
+  //       this.potiong = 255;
+  //       this.potionb = 255;
+  //     }
+  //     else if (this.potionb > this.potionr) {
+  //       health += 10;
+  //       this.potionb = 255;
+  //       this.potiong = 255;
+  //       this.potionr = 255;
+  //     }
+  //   }
 }
 
 function playerHealth() {
-  if (characterXPos > 300 || characterYPos > 300) {
-    health -= 0.05;
+  // let xcoord = floor(this.poisonlft / this.poisonradius);
+  // let ycoord = floor(this.poisontop / this.poisonradius);
+  if (characterXPos >= 300 || characterYPos >= 300) {
+    health -= 1;
+    print(health);
   }
+  // else if (characterXPos === xcoord && characterYPos === ycoord) {
+  //   health -= 1;
+  // }
   if (health < 1) {
     fill(0);
     rect(characterXPos, characterYPos, 30, 30);
   }
 }
-
-// characterXPos >= this.potionlft && characterXPos <= this.potionrgt && characterYPos >= this.potiontop && characterYPos <= this.potionbot
